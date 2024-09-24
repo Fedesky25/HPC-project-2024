dea630b719fe83d2b17d0bc5f31b009c9240c5f6
Fedesky25 <fedesky.guglielmi@gmail.com>
2024-09-24 11:05:00 +0200

    fix: mail, and folders creation

--- launch.sh
+++ launch.sh
@@ -5,14 +5,19 @@
 #SBATCH --mem=10GB
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cuda
-#SBATCH --mail-type=all
+#SBATCH --mail-type=ALL
 #SBATCH --mail-user=s328789@studenti.polito.it
+
+echo $(date +"%D %T")
+echo -e "\n\n\n\n"
 
 module load nvidia/cudasdk/11.8
 module load cmake/3.14.3
 module load ffmpeg/4.3.4
 
-mkdir build
+mkdir -p build
+if [ -d "./source" ]; then rm -r ./source; fi
+
 git clone https://github.com/Fedesky25/HPC-project-2024.git source
 cmake -DCMAKE_BUILD_TYPE=Release ./source
 cmake --build ./build --target HPC_project_2024 -j 10
