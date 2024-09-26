9e8c6bc354abb14b4d441176e1e19f00c28ef4b0
Fedesky25 <fedesky.guglielmi@gmail.com>
2024-09-26 15:36:39 +0200

    chore(evolution): sync device after evolution kernel

--- src/evolution.cu
+++ src/evolution.cu
@@ -73,7 +73,7 @@ void evolve_gpu(Configuration * config,
     auto d_config = devicify(config);
     evolve_kernel<<<canvas_count, tiles_count>>>(d_config, canvas, particles, tile_offsets, d_rand_floats, func);
     cudaFree(d_config);
-//    cudaDeviceSynchronize();
+    cudaDeviceSynchronize();
     tock_s(0);
     std::cout << "Particle evolution computed in " << t_elapsed << 's' << std::endl;
 }
