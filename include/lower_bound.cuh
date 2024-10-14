64c58220e740a04d90c366c4465e8fcae5d3a4bd
Fedesky25 <fedesky.guglielmi@gmail.com>
2024-10-14 14:25:09 +0200

    feat: upper_bound

--- include/lower_bound.cuh
+++ include/lower_bound.cuh
@@ -37,4 +37,34 @@ __device__ Index lower_bound(T value, T * data, Index length) {
     return first;
 }
 
+/**
+ * Searches for the first element x such that value < x
+ * @tparam T type of array element
+ * @tparam Index integral type used to represent indexes
+ * @param value value to search for
+ * @param data array
+ * @param length m_length of the array
+ * @return index of the first element x such that x <= value
+ * @see https://en.cppreference.com/w/cpp/algorithm/upper_bound
+ */
+template<class T, class Index>
+__device__ Index upper_bound(T value, T * data, Index length) {
+//    static_assert(std::is_integral_v<Index>, "Type used as index must be of integral type");
+    Index step, index, first = 0;
+    while (length > 0)
+    {
+        index = first;
+        step = length >> 1;
+        index += step;
+        if (data[index] <= value)
+        {
+            first = index + 1;
+            length -= step + 1;
+        }
+        else length = step;
+    }
+
+    return first;
+}
+
 #endif //HPC_PROJECT_2024_LOWER_BOUND_CUH
