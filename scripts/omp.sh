37a1676fd54d3ba3ce2237ddc71833b48e1facce
Fedesky25 <fedesky.guglielmi@gmail.com>
2024-11-14 18:50:47 +0100

    chore(scripts): omp version benchmark

--- scripts/omp.sh
+++ scripts/omp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash +x
 #SBATCH --job-name="CFS (OpenMP)"
-#SBATCH --time=15:00
+#SBATCH --time=30:00
 #SBATCH --mem=5GB
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -18,8 +18,15 @@ if [[ ! -e "./build/HPC_project_2024" ]]
   exit
 fi
 
-module load ffmpeg/4.3.4
+# module load ffmpeg/4.3.4
 cd ./build
 
-./HPC_project_2024 -p omp -d 3 -o sin.raw sin
-./HPC_project_2024 -p omp -d 3 -o pow.raw -n 4 ^n
+num_threads=(4,8,16,32,64)
+for num in "${num_threads[@]}"; do
+  export OMP_NUM_THREADS $num
+  echo "================================================================================ threads = $num"
+  for i in $(seq 1 100); do
+    echo ">> iteration $i/100"
+    ./HPC_project_2024 -p omp -R FHD -D 10 -f 60 -s 4u/h -d 12 poly1
+  done
+done
