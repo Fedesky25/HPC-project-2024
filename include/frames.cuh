339641c690503616d710288abf816f33549d3670
Fedesky25 <fedesky.guglielmi@gmail.com>
2025-04-07 19:51:32 +0200

    feat(frame): computer functions

--- include/frames.cuh
+++ include/frames.cuh
@@ -6,22 +6,77 @@
 #define HPC_PROJECT_2024_FRAMES_CUH
 
 #include "canvas.cuh"
-#include <cstdint>
+#include "libavutil/frame.h"
+
+
+/**
+ * Computes the frame at a specific time serially
+ * @param time time instant in range [0, frame_count)
+ * @param frame_count total number of frames in range [0, UINT16_MAX)
+ * @param lifetime lifetime of a particle
+ * @param canvas the canvas
+ * @param frame output frame (host) buffer
+ * @param background background YUVA color
+ */
+template<bool opaque>
+void compute_frame_serial(
+        int32_t time, int32_t frame_count, int32_t lifetime,
+        const Canvas& canvas, AVFrame * frame, const YUVA * background);
 
 
 /**
  * Computes the frame at a specific time using OpenMP
  * @param time time instant in range [0, frame_count)
  * @param frame_count total number of frames in range [0, UINT16_MAX)
+ * @param lifetime lifetime of a particle
  * @param canvas_array (host) array of canvases
  * @param canvas_count number of canvases
  * @param frame output frame (host) buffer
- * @param size total number of pixels
+ * @param background background YUVA color
  */
-void compute_frame_omp(int32_t time, int32_t frame_count,
-                       const Canvas * canvas_array, unsigned canvas_count,
-                       uint32_t * frame, uint32_t size,
-                       const RGBA * background);
+template<bool opaque>
+void compute_frame_omp(
+        int32_t time, int32_t frame_count, int32_t lifetime,
+        const Canvas * canvas_array, unsigned canvas_count,
+        AVFrame * frame, const YUVA * background);
+
+
+/** Struct containing the constant arguments the gpu kernel needs to compute a frame */
+struct FrameKernelArguments {
+    int32_t frame_count, lifetime;
+    const Canvas * canvas_array;
+    unsigned canvas_count;
+    YUVA background;
+
+    uint8_t * channels[4];
+    int line_size[4];
+    int width, height;
+
+    /**
+     * Allocates the channels and line_size on the gpu
+     * @param frame reference (allocated) frame
+     * @return a copy of this instance on the gpu
+     */
+    const FrameKernelArguments * init(AVFrame * frame, bool opaque);
+
+    /**
+     * Copies the contents into the given frame
+     * @param frame
+     */
+    void copy_into(AVFrame * frame) const;
+
+    /** Frees up the allocated channels */
+    void free();
+};
+
+/**
+ * Computes the frame at a specific time using the GPU
+ * @tparam opaque whether the video has opaque background or not
+ * @param time current simulation time index
+ * @param args pointer to arguments allocated on the gpu
+ */
+template<bool opaque>
+__global__ void compute_frame_kernel(int32_t time, const FrameKernelArguments * args);
 
 /**
  * Computes the frame at a specific time using the GPU
--- src/frames.cu
+++ src/frames.cu
@@ -5,25 +5,165 @@
 #include "frames.cuh"
 
 
-void compute_frame_omp(
-        int32_t time, int32_t frame_count,
-        const Canvas * canvas_array, unsigned canvas_count,
-        uint32_t * frame, uint32_t size,
-        const RGBA * background
+template<bool opaque>
+BOTH void write_color(const YUVA & clr, AVFrame * frame, int x, int y) {
+    frame->data[0][x + y*frame->linesize[0]] = byte_clr_chl1(clr.Y);
+    frame->data[1][x + y*frame->linesize[1]] = byte_clr_chl2(clr.U);
+    frame->data[2][x + y*frame->linesize[2]] = byte_clr_chl2(clr.V);
+    CONSTEXPR_IF(opaque) frame->data[3][x + y*frame->linesize[3]] = byte_clr_chl1(clr.A);
+}
+
+
+template<bool opaque>
+void compute_frame_serial(
+        int32_t time, int32_t frame_count, int32_t lifetime,
+        const Canvas& canvas, AVFrame * frame, const YUVA * background
 ) {
-    #pragma omp parallel for schedule(static)
-    for(int32_t i=0; i<size; i++) {
-        unsigned selected_canvas = 0;
-        int32_t time_delta, selected_time_delta = canvas_array[0][i].time_distance(time, frame_count);
-        for(unsigned c=1; c<canvas_count; c++) {
-            time_delta = canvas_array[c][i].time_distance(time, frame_count);
-            if(time_delta < selected_time_delta) {
-                selected_time_delta = time_delta;
-                selected_canvas = c;
+    YUVA brush;
+    auto inv_lifetime = 1.0f / (float) lifetime;
+    uint8_t bytes_bg[] = {
+            byte_clr_chl1(background->Y),
+            byte_clr_chl2(background->U),
+            byte_clr_chl2(background->V),
+            byte_clr_chl1(background->A),
+    };
+    for (int y = 0; y < frame->height; y++) {
+        for (int x = 0; x < frame->width; x++) {
+            int i = x + y*frame->width;
+            auto delta = canvas[i].time_distance(time, frame_count) - canvas[i].multiplicity;
+            if(delta >= lifetime) {
+                frame->data[0][x + y*frame->linesize[0]] = bytes_bg[0];
+                frame->data[1][x + y*frame->linesize[1]] = bytes_bg[1];
+                frame->data[2][x + y*frame->linesize[2]] = bytes_bg[2];
+                CONSTEXPR_IF(opaque) frame->data[3][x + y*frame->linesize[3]] =  bytes_bg[3];
+            }
+            else {
+                brush.from_hue(canvas[i].hue);
+                brush.A = 1.0f;
+                if (delta >= 0) {
+                    brush.A -= (float) delta * inv_lifetime;
+                    brush.over<opaque>(background);
+                }
+                write_color<opaque>(brush, frame, x, y);
             }
         }
-        auto px = &canvas_array[selected_canvas][i];
-        //frame[i] = px->get_color(selected_time_delta, frame_count, background);
+    }
+}
+
+
+template<bool opaque>
+void compute_frame_omp(
+        int32_t time, int32_t frame_count, int32_t lifetime,
+        const Canvas * canvas_array, unsigned canvas_count,
+        AVFrame * frame, const YUVA * background
+) {
+    YUVA brush;
+    auto inv_lifetime = 1.0f / (float) lifetime;
+    uint8_t bytes_bg[] = {
+            byte_clr_chl1(background->Y),
+            byte_clr_chl2(background->U),
+            byte_clr_chl2(background->V),
+            byte_clr_chl1(background->A),
+    };
+    #pragma omp parallel for schedule(static) private(brush)
+    for (int y = 0; y < frame->height; y++) {
+        for (int x = 0; x < frame->width; x++) {
+            int i = x + y*frame->width;
+            unsigned selected_canvas = 0;
+            int32_t dt, time_delta = canvas_array[0][i].time_distance(time, frame_count);
+            for(unsigned c=1; c<canvas_count; c++) {
+                dt = canvas_array[c][i].time_distance(time, frame_count);
+                if(dt < time_delta) {
+                    time_delta = dt;
+                    selected_canvas = c;
+                }
+            }
+            auto pixel = canvas_array[selected_canvas][i];
+            time_delta -= pixel.multiplicity;
+            if(time_delta >= lifetime) {
+                frame->data[0][x + y*frame->linesize[0]] = bytes_bg[0];
+                frame->data[1][x + y*frame->linesize[1]] = bytes_bg[1];
+                frame->data[2][x + y*frame->linesize[2]] = bytes_bg[2];
+                CONSTEXPR_IF(opaque) frame->data[3][x + y*frame->linesize[3]] =  bytes_bg[3];
+            }
+            else {
+                brush.from_hue(pixel.hue);
+                brush.A = 1.0f;
+                if (time_delta >= 0)
+                    brush.A -= (float) time_delta * inv_lifetime;
+                brush.over<opaque>(background);
+            }
+            write_color<opaque>(brush, frame, x, y);
+        }
+    }
+}
+
+const FrameKernelArguments * FrameKernelArguments::init(AVFrame *frame, bool opaque) {
+    width = frame->width;
+    height = frame->height;
+    for(int i=0; i < 3+!opaque; i++) {
+        line_size[i] = frame->linesize[i];
+        auto size = frame->linesize[i] * height;
+        CATCH_CUDA_ERROR(cudaMalloc(channels+i,size))
+    }
+    if(opaque) channels[3] = nullptr;
+    FrameKernelArguments * this_d;
+    CATCH_CUDA_ERROR(cudaMalloc(&this_d, sizeof(FrameKernelArguments)))
+    CATCH_CUDA_ERROR(cudaMemcpy(this_d, this, sizeof(FrameKernelArguments), cudaMemcpyHostToDevice));
+    return this_d;
+}
+
+void FrameKernelArguments::copy_into(AVFrame *frame) const {
+    auto N = channels[3] == nullptr ? 3 : 4;
+    for(int i=0; i < N; i++) {
+        auto size = frame->linesize[i] * height;
+        CATCH_CUDA_ERROR(cudaMemcpy(frame->data[i], channels[i], size, cudaMemcpyDeviceToHost))
+    }
+}
+
+void FrameKernelArguments::free() {
+    auto N = channels[3] == nullptr ? 3 : 4;
+    for(int i=0; i < N; i++) CATCH_CUDA_ERROR(cudaFree(channels[i]));
+}
+
+
+template<bool opaque>
+__global__ void compute_frame_kernel(int32_t time, const FrameKernelArguments * args) {
+    auto x = threadIdx.x + blockIdx.x * blockDim.x;
+    auto y = threadIdx.y + blockIdx.y * blockDim.y;
+    if(x >= args->width || y >= args->height) return;
+    auto pixel_index = x + y * args->width;
+    unsigned selected_canvas = 0;
+    int32_t dt, time_delta = args->canvas_array[0][pixel_index].time_distance(time, args->frame_count);
+    for(unsigned i=1; i<args->canvas_count; i++) {
+        dt = args->canvas_array[i][pixel_index].time_distance(time, args->frame_count);
+        if(dt < time_delta) {
+            time_delta = dt;
+            selected_canvas = i;
+        }
+        // cannot sync because of forking
+        // __syncthreads();
+    }
+    auto px = args->canvas_array[selected_canvas][pixel_index];
+    time_delta -= px.multiplicity;
+    if(time_delta >= args->lifetime) {
+        args->channels[0][x + y*args->line_size[0]] = byte_clr_chl1(args->background.Y);
+        args->channels[1][x + y*args->line_size[1]] = byte_clr_chl2(args->background.U);
+        args->channels[2][x + y*args->line_size[2]] = byte_clr_chl2(args->background.V);
+        CONSTEXPR_IF(opaque) args->channels[3][x + y*args->line_size[3]] = byte_clr_chl1(args->background.A);
+    }
+    else {
+        YUVA color;
+        color.from_hue(px.hue);
+        color.A = 1.0f;
+        if(time_delta >= 0) {
+            color.A -= (float) time_delta / (float) args->lifetime;
+            color.over<opaque>(args->background);
+        }
+        args->channels[0][x + y*args->line_size[0]] = byte_clr_chl1(color.Y);
+        args->channels[1][x + y*args->line_size[1]] = byte_clr_chl2(color.U);
+        args->channels[2][x + y*args->line_size[2]] = byte_clr_chl2(color.V);
+        CONSTEXPR_IF(opaque) args->channels[3][x + y*args->line_size[3]] = byte_clr_chl1(color.A);
     }
 }
 
