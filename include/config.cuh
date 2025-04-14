a05547e73d080034e4e5aed8e7482f639f618194
Fedesky25 <fedesky.guglielmi@gmail.com>
2025-04-14 13:57:19 +0200

    fix(color): default color value

--- include/config.cuh
+++ include/config.cuh
@@ -75,7 +75,7 @@ struct Configuration {
     FnVariables vars;
     CanvasAdapter canvas;
     EvolutionOptions evolution;
-    YUVA background{0.135f, 0.510f, 0.5f, 1};
+    YUVA background{0.186f, 0.02f, 0.f, 1};
 //    FixedHSLA background{ IC_16b, icenc(0.06), icenc(0.15), ICE_1 };
     const char * output = "plot.mp4";
     unsigned long particle_distance = 10;
