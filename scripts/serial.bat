e96f96329f182ffea4897eb2b4226491476e85dd
Fedesky25 <fedesky.guglielmi@gmail.com>
2025-04-26 15:11:01 +0200

    chore(scripts): bat files

---
+++ scripts/serial.bat
@@ -0,0 +1,20 @@
+echo.
+echo =========================================================== Fibonacci(z)
+for /L %%i in (1,1,20) do (
+  echo # %i%/20
+  .\HPC_project_2024 -p none -o videos\serial-fib.mp4 -R qHD -D 2 -L 5 fib
+)
+
+echo.
+echo =========================================================== exp(z^3)
+for /L %%i in (1,1,20) do (
+  echo # %i%/20
+  .\HPC_project_2024 -p none -o videos\serial-cubic.mp4 -R qHD -D 2 -n 3 -L 5 exp^n
+)
+
+echo.
+echo =========================================================== Gamma(z)
+for /L %%i in (1,1,20) do (
+  echo # %i%/20
+  .\HPC_project_2024 -p none -o videos\serial-gamma.mp4 -R qHD -D 2 -L 5 -v 0.1 gamma
+)
