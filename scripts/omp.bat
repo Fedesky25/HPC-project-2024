4257f983bdc9b20248c68888373c615402eabd4d
Fedesky25 <fedesky.guglielmi@gmail.com>
2025-04-26 15:17:56 +0200

    fix(scripts): bat echo

--- scripts/omp.bat
+++ scripts/omp.bat
@@ -1,20 +1,25 @@
+@echo off
+
 echo.
 echo =========================================================== Fibonacci(z)
 for /L %%i in (1,1,20) do (
-  echo # %i%/20
+  echo.
+  echo # %%i/20
   .\HPC_project_2024 -p omp -o videos\serial-fib.mp4 -R qHD -D 2 -L 5 fib
 )
 
 echo.
 echo =========================================================== exp(z^3)
 for /L %%i in (1,1,20) do (
-  echo # %i%/20
+  echo.
+  echo # %%i/20
   .\HPC_project_2024 -p omp -o videos\serial-cubic.mp4 -R qHD -D 2 -n 3 -L 5 exp^n
 )
 
 echo.
 echo =========================================================== Gamma(z)
 for /L %%i in (1,1,20) do (
-  echo # %i%/20
+  echo.
+  echo # %%i/20
   .\HPC_project_2024 -p omp -o videos\serial-gamma.mp4 -R qHD -D 2 -L 5 -v 0.1 gamma
 )
