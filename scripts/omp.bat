92de799bfe187fd67c5a38bd10e9e5d2266a481c
Fedesky25 <fedesky.guglielmi@gmail.com>
2025-05-19 10:51:41 +0200

    chore(scripts): windows omp loop through thread numbers

--- scripts/omp.bat
+++ scripts/omp.bat
@@ -1,25 +1,31 @@
 @echo off
 
-echo.
-echo =========================================================== Fibonacci(z)
-for /L %%i in (1,1,20) do (
-  echo.
-  echo # %%i/20
-  .\HPC_project_2024 -p omp -o videos\omp-fib.mp4 -R qHD -D 2 -L 5 fib
-)
+for %%num in (3, 6, 12) do (
+    echo.
+    set OMP_NUM_THREADS=%%num
+    echo ================================================================================ threads = %%num
 
-echo.
-echo =========================================================== exp(z^3)
-for /L %%i in (1,1,20) do (
-  echo.
-  echo # %%i/20
-  .\HPC_project_2024 -p omp -o videos\omp-cubic.mp4 -R qHD -D 2 -n 3 -L 5 "exp^n"
-)
+    echo.
+    echo =========================================================== Fibonacci(z)
+    for /L %%i in (1,1,20) do (
+      echo.
+      echo # %%i/20
+      .\HPC_project_2024 -p omp -o videos\omp-fib.mp4 -R qHD -D 2 -L 5 fib
+    )
 
-echo.
-echo =========================================================== Gamma(z)
-for /L %%i in (1,1,20) do (
-  echo.
-  echo # %%i/20
-  .\HPC_project_2024 -p omp -o videos\omp-gamma.mp4 -R qHD -D 2 -L 5 --speed 0.1 gamma
+    echo.
+    echo =========================================================== exp(z^3)
+    for /L %%i in (1,1,20) do (
+      echo.
+      echo # %%i/20
+      .\HPC_project_2024 -p omp -o videos\omp-cubic.mp4 -R qHD -D 2 -n 3 -L 5 "exp^n"
+    )
+
+    echo.
+    echo =========================================================== Gamma(z)
+    for /L %%i in (1,1,20) do (
+      echo.
+      echo # %%i/20
+      .\HPC_project_2024 -p omp -o videos\omp-gamma.mp4 -R qHD -D 2 -L 5 --speed 0.1 gamma
+    )
 )
