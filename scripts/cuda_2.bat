b8e1e93098a6879c78e1a657765aeb3b152aa9ca
Sofyh02 <grandisofia29@gmail.com>
2025-05-20 20:15:16 +0200

    fix(scripts): cuda new script

--- scripts/cuda_2.bat
+++ scripts/cuda_2.bat
@@ -1,7 +1,14 @@
 @echo off
 
-echo.
-echo =========================================================== Fibonacci(z)
-  echo.
-  .\HPC_project_2024 -p gpu -R FHD -D 1 -f 20 -L 5 -d 9 fib
+for %%n in (39, 23, 18, 15, 13, 12, 11, 10) do (
+
+    echo.
+    echo =========================================================== d = %%n
+
+    for /L %%i in (1,1,20) do (
+        echo.
+        echo # %%i/20
+        .\HPC_project_2024 -p gpu -R FHD -D 1 -f 20 -L 5 -d %%n fib
+    )
+
 )
