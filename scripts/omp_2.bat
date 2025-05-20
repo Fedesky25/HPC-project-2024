1b517634cf6abf352c8bc83f7ec23772154b37b1
Sofyh02 <grandisofia29@gmail.com>
2025-05-20 13:32:25 +0200

    fix(scripts): omp new script

--- scripts/omp_2.bat
+++ scripts/omp_2.bat
@@ -6,12 +6,12 @@ SETLOCAL ENABLEDELAYEDEXPANSION
     echo.
     echo ================================================================================ threads = 6
 
-    for %%n in (39, 32, 28, 24, 22, 20, 19, 18) do (
+    for %%n in (39, 28, 23, 20, 18, 16, 15, 14) do (
 
         echo.
         echo =========================================================== d = %%n
 
-        for /L %%i in (1,1,20) do(
+        for /L %%i in (1,1,20) do (
             echo.
             echo # %%i/20
             .\HPC_project_2024 -p omp -R FHD -D 1 -f 20 -L 5 -d %%n fib
