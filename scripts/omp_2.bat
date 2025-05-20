6f34e626f59ea58f4deb65b843a1b86ccef4d63a
Fedesky25 <fedesky.guglielmi@gmail.com>
2025-05-20 13:22:46 +0200

    refactor(scripts): new omp testing script

--- scripts/omp_2.bat
+++ scripts/omp_2.bat
@@ -1,14 +1,22 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-set OMP_NUM_THREADS=12
-echo.
-    echo ================================================================================ threads = 12
 
+    set OMP_NUM_THREADS= 6
     echo.
-    echo =========================================================== Fibonacci^(z^)
-      echo.
-      .\HPC_project_2024 -p omp -R FHD -D 1 -f 20 -L 5 -d 12 fib
+    echo ================================================================================ threads = 6
+
+    for %%n in (39, 32, 28, 24, 22, 20, 19, 18) do (
+
+        echo.
+        echo =========================================================== d = %%n
+
+        for /L %%i in (1,1,20) do(
+            echo.
+            echo # %%i/20
+            .\HPC_project_2024 -p omp -R FHD -D 1 -f 20 -L 5 -d %%n fib
+        )
+
     )
 
 ENDLOCAL
